fcad42a426df93a94bf865a3a8e00046ccaf3fcf
anantadhuaheryanty <anantadhuaheryanty@gmail.com>
2025-09-02 05:20:13 +0000

    pertemuan ke-2

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Ananta-2307051001'),
+      home: const MyHomePage(title: 'Ananta Dhua Heryanty-2307051001'),
     );
   }
 }
